60a314e73da8ff5dbfc70fb2dd04b2e08f2bd170
Bhautik Gondaliya <mr.genius0180@gmail.com>
2023-04-21 20:00:17 +0530

    mail security checking changes last

--- pms/pms/settings.py
+++ pms/pms/settings.py
@@ -143,5 +143,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587 
-EMAIL_HOST_USER = 'bhautikgondaliya6979@gmail.com'
-EMAIL_HOST_PASSWORD = 'saklfosrcpgbzinu'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
